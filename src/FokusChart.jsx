70d9a7f603668cf98b79f5e171dec08d61bf229b
Siro6503 <siro.gentilini@students.fhnw.ch>
2025-12-22 08:25:36 +0100

    Backend_fokusfrage

--- src/FokusChart.jsx
+++ src/FokusChart.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { VegaEmbed } from "react-vega";
-
-// import dello spec generato da Altair
-import fokusfrage from "./json_spec/fokusfrage.json";
+import fokusfrageBase from "./json_spec/fokusfrage.json";
 
 export const FokusChart = () => {
+  const [spec, setSpec] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/locations/331/focus")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log("Dati backend Fokusfrage:", json);
+
+        const newSpec = {
+          ...fokusfrageBase,
+          data: {
+            values: json, // qui inseriamo i dati del backend
+          },
+        };
+
+        setSpec(newSpec);
+      })
+      .catch((err) => {
+        console.error("Errore fetch Fokusfrage", err);
+      });
+  }, []);
+
+  if (!spec) {
+    return <div>Lade Fokusfrage Diagramm…</div>;
+  }
+
   return (
     <div>
-      <VegaEmbed spec={fokusfrage} />
+      <VegaEmbed spec={spec} />
     </div>
   );
 };
